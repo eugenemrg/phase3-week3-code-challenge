8be7b27193b148245e80d499cc1fe19bbfc44f95
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-09-03 22:48:53 +0300

    Added the review model with association using an Association Object, generated Review table

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
-from sqlalchemy import (Column, Integer, String)
+from sqlalchemy import ForeignKey, Table, Column, Integer, String
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.associationproxy import association_proxy
 
 engine = create_engine('sqlite:///reviewer.db')
 
@@ -12,6 +14,9 @@ class Restaurant(Base):
     id = Column(Integer(), primary_key=True)
     name = Column(String(), index=True)
     price = Column(Integer())
+    
+    reviews = relationship('Review', back_populates='restaurant')
+    users = association_proxy('reviews', 'customer', creator=lambda cust: Review(customer=cust))
     
     def __repr__(self):
         return f"Restaurant {self.id}: " \
@@ -25,7 +30,27 @@ class Customer(Base):
     first_name = Column(String(), index=True)
     last_name = Column(String(), index=True)
     
+    reviews = relationship('Review', back_populates='customer')
+    restaurants = association_proxy('reviews', 'restaurant', creator=lambda res: Review(restaurant=res))
+    
     def __repr__(self):
         return f"Customer {self.id}: " \
             + f"{self.first_name} " \
             + f"{self.last_name}"
+
+class Review(Base):
+    __tablename__ = 'reviews'
+
+    id = Column(Integer(), primary_key=True)
+    star_rating = Column(Integer())
+    restaurant_id = Column(Integer(), ForeignKey('restaurants.id'))
+    customer_id = Column(Integer(), ForeignKey('customers.id'))
+    
+    customer = relationship('Customer', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
+
+    def __repr__(self):
+        return f'Review(id={self.id}, ' + \
+            f'star rating={self.star_rating}, ' + \
+            f'restaurant id={self.restaurant_id}, ' + \
+            f'customer id={self.customer_id})'
