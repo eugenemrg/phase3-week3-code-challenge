1e8fef6357a3e6c46ca89214152f4a89cc483b47
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-09-03 17:17:10 +0300

    Created a base, empty migration, created database

---
+++ app/migrations/versions/40fe7eedd984_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 40fe7eedd984
+Revises: 
+Create Date: 2023-09-03 17:14:37.383949
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '40fe7eedd984'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
